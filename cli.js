5726ff8250ba32ba73fcba1bb4d67191dc5c76d0
akameco <akameco.t@gmail.com>
2017-10-05 03:57:35 +0900

    Format by prettier

--- cli.js
+++ cli.js
@@ -1,27 +1,25 @@
 #!/usr/bin/env node
-'use strict';
-const meow = require('meow');
-const updateNotifier = require('update-notifier');
-const prioritizeYarn = require('./');
+'use strict'
+const meow = require('meow')
+const updateNotifier = require('update-notifier')
+const prioritizeYarn = require('./')
 
-const cli = meow({
-	help: false,
-	version: false
-}, {
-	boolean: [
-		'save',
-		'save-dev',
-		'save-optional',
-		'save-exact'
-	],
-	alias: {
-		S: 'save',
-		D: 'save-dev',
-		O: 'save-optional',
-		E: 'save-exact'
-	}
-});
+const cli = meow(
+  {
+    help: false,
+    version: false,
+  },
+  {
+    boolean: ['save', 'save-dev', 'save-optional', 'save-exact'],
+    alias: {
+      S: 'save',
+      D: 'save-dev',
+      O: 'save-optional',
+      E: 'save-exact',
+    },
+  }
+)
 
-updateNotifier({pkg: cli.pkg}).notify();
+updateNotifier({ pkg: cli.pkg }).notify()
 
-prioritizeYarn(cli.input, cli.flags);
+prioritizeYarn(cli.input, cli.flags)
--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-'use strict';
-const execa = require('execa');
-const hasYarn = require('has-yarn');
+'use strict'
+const execa = require('execa')
+const hasYarn = require('has-yarn')
 
 function isInstall(input) {
-	return input === 'i' || input === 'install';
+  return input === 'i' || input === 'install'
 }
 
 function hasNoFlags(flags) {
-	// Meow sets all flags to false unless present, so we need to check all of them
-	return !Object.keys(flags).reduce((hasFlag, key) => hasFlag || flags[key], false);
+  // Meow sets all flags to false unless present, so we need to check all of them
+  return !Object.keys(flags).reduce(
+    (hasFlag, key) => hasFlag || flags[key],
+    false
+  )
 }
 
 module.exports = (input, flags) => {
-	if (!Array.isArray(input)) {
-		return Promise.reject(new TypeError(`Expected a Array, got ${typeof input}`));
-	}
+  if (!Array.isArray(input)) {
+    return Promise.reject(
+      new TypeError(`Expected a Array, got ${typeof input}`)
+    )
+  }
 
-	flags = flags || {};
+  flags = flags || {}
 
-	let task = 'npm';
-	let args = process.argv.slice(2);
-	if (isInstall(input[0]) && hasYarn()) {
-		task = 'yarn';
-		if (flags.save) {
-			args = ['add'].concat(input.slice(1));
-		} else if (flags.saveDev) {
-			args = ['add', '--dev'].concat(input.slice(1));
-		} else if (flags.saveOptional) {
-			args = ['add', '--optional'].concat(input.slice(1));
-		} else if (flags.saveExact) {
-			args = ['add', '--exact'].concat(input.slice(1));
-		} else if (hasNoFlags(flags)) {
-			args = [];
-		} else {
-			// For npm install --global and more...
-			task = 'npm';
-		}
-	}
-	const opts = {
-		cwd: process.cwd(),
-		stdio: 'inherit'
-	};
+  let task = 'npm'
+  let args = process.argv.slice(2)
+  if (isInstall(input[0]) && hasYarn()) {
+    task = 'yarn'
+    if (flags.save) {
+      args = ['add'].concat(input.slice(1))
+    } else if (flags.saveDev) {
+      args = ['add', '--dev'].concat(input.slice(1))
+    } else if (flags.saveOptional) {
+      args = ['add', '--optional'].concat(input.slice(1))
+    } else if (flags.saveExact) {
+      args = ['add', '--exact'].concat(input.slice(1))
+    } else if (hasNoFlags(flags)) {
+      args = []
+    } else {
+      // For npm install --global and more...
+      task = 'npm'
+    }
+  }
+  const opts = {
+    cwd: process.cwd(),
+    stdio: 'inherit',
+  }
 
-	return execa(task, args, opts).catch(() => {});
-};
+  return execa(task, args, opts).catch(() => {})
+}

c197f8f538783d1460d4d779595e7cf1c7e17b45
akameco <akameco.t@gmail.com>
2019-02-11 20:48:23 +0900

    feat(uninstall): support remove flag

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const execa = require('execa')
 const hasYarn = require('has-yarn')
 
 function isInstall(input) {
-  return input === 'i' || input === 'install'
+  return ['i', 'install'].includes(input)
+}
+
+function isUninstall(input) {
+  return ['uninstall', 'remove', 'rm', 'r', 'un', 'unlink'].includes(input)
 }
 
 function hasNoFlags(flags) {
@@ -14,6 +18,15 @@ function hasNoFlags(flags) {
   )
 }
 
+function getFlag(input) {
+  if (isInstall(input)) {
+    return 'add'
+  } else if (isUninstall(input)) {
+    return 'remove'
+  }
+  return null
+}
+
 module.exports = (input, flags) => {
   if (!Array.isArray(input)) {
     return Promise.reject(
@@ -22,29 +35,35 @@ module.exports = (input, flags) => {
   }
 
   flags = flags || {}
+  const opts = {
+    cwd: process.cwd(),
+    stdio: 'inherit'
+  }
 
   let task = 'npm'
   let args = process.argv.slice(2)
-  if (isInstall(input[0]) && hasYarn()) {
+
+  if (!hasYarn()) {
+    return execa(task, args, opts)
+  }
+
+  if (isInstall(input[0]) || isUninstall(input[0])) {
     task = 'yarn'
+    const flag = getFlag(input[0])
     if (flags.save) {
-      args = ['add'].concat(input.slice(1))
+      args = [flag].concat(input.slice(1))
     } else if (flags.saveDev) {
-      args = ['add', '--dev'].concat(input.slice(1))
+      args = [flag, '--dev'].concat(input.slice(1))
     } else if (flags.saveOptional) {
-      args = ['add', '--optional'].concat(input.slice(1))
+      args = [flag, '--optional'].concat(input.slice(1))
     } else if (flags.saveExact) {
-      args = ['add', '--exact'].concat(input.slice(1))
+      args = [flag, '--exact'].concat(input.slice(1))
     } else if (hasNoFlags(flags)) {
       args = []
     } else {
       // For npm install --global and more...
       task = 'npm'
     }
-  }
-  const opts = {
-    cwd: process.cwd(),
-    stdio: 'inherit'
   }
 
   return execa(task, args, opts).catch(() => {})
